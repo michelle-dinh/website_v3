9bf27776d43c57d4444da52a38ae093e746247c1
Michelle Dinh <michelle.dinh96@yahoo.com>
2019-11-26 06:10:51 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var song, fft, analyzer, mic, radius, pieces, mapMouseX, mapMouseY;
+var song, fft, analyzer, mic;
 //creating an array for color palette (dark brown, beige, cream, reddish)
 var colorPalette = ["⁣⁣⁣⁣#312D2F", "⁣⁣⁣⁣#E0D5C7", "⁣⁣⁣⁣#F6F1EB", "⁣⁣⁣⁣#D36462"];
 function preload() {
@@ -12,85 +12,29 @@ function setup() {
   mic = new p5.AudioIn();
   mic.start();
 
+  analyzer.setInput(song);
+
 }
 
 function draw() {
 
 background(colorPalette[3]);
+
 translate(windowWidth/2, windowHeight/2);
 
 level = analyzer.getLevel();
 fft.analyze();
 let vol = mic.getLevel();
 
-var bass = fft.getEnergy(100, 150);
-  var treble = fft.getEnergy(150, 250);
-  var mid = fft.getEnergy("mid");
+let rms = analyzer.getLevel();
+  fill(127);
+  stroke(0);
 
-  var mapMid = map(mid, 0, 255, -100, 200);
-  var scaleMid = map(mid, 0, 255, 1, 1.5);
-
-  var mapTreble = map(treble, 0, 255, 200, 350);
-  var scaleTreble = map(treble, 0, 255, 0, 1);
-
-  var mapbass = map(bass, 0, 255, 50, 200);
-  var scalebass = map(bass, 0, 255, 0.05, 1.2);
-
-  mapMouseX = map(mouseX, 0, width, 1, 50);
-  mapMouseXbass = map(mouseX, 0, width, 1, 5);
-  mapMouseY = map(mouseY, 0, height, 2, 6);
-
-  pieces = 20;
-  radius = 100;
-
-  for (i = 0; i < pieces; i += 0.1) {
-
-    rotate(TWO_PI / (pieces / 2));
-
-    noFill();
-
-    /*----------  BASS  ----------*/
-    push();
-    stroke(colorPalette[1]);
-    rotate(frameCount * 0.002);
-    strokeWeight(0.5);
-    polygon(mapbass + i, mapbass - i, mapMouseXbass * i, 3);
-    pop();
-
-
-    /*----------  MID  ----------*/
-    push();
-    stroke(colorPalette[2]);
-    strokeWeight(0.2);
-    polygon(mapMid + i / 2, mapMid - i * 2, mapMouseX * i, 7);
-    pop();
-
-
-    /*----------  TREMBLE  ----------*/
-    push();
-    stroke(colorPalette[3]);
-    strokeWeight(0.6);
-    scale(mouseX * 0.0005);
-    rotate((mouseX * 0.002));
-    polygon(mapTreble + i / 2, mapTreble - i / 2, mapMouseY * i / 2, 3);
-    pop();
-
-  }
-
-
+  // Draw an ellipse with size based on volume
+  ellipse(width / 2, height / 2, 10 + rms * 200, 10 + rms * 200);
 }
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function polygon(x, y, radius, npoints) {
-  var angle = TWO_PI / npoints;
-  beginShape();
-  for (var a = 0; a < TWO_PI; a += angle) {
-    var sx = x + cos(a) * radius;
-    var sy = y + sin(a) * radius;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
 }
