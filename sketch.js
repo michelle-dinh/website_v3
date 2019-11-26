b557ab051400468060539fb36688eb875e5e9634
Michelle Dinh <michelle.dinh96@yahoo.com>
2019-11-26 07:14:09 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ let rms = analyzer.getLevel();
   push();
   angle = angle + rms;
   rectMode(CENTER);
-  rotate(frameCount * a);
+  rotate(a / 2);
   // Draw an ellipse with size based on volume
   rect(0, 0, 20 + rms * 200, 20 + rms * 200);
   pop();
